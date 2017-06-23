7773a78c7b8c0c1e64891d09943ba331487df96c
Ali Zhdanov <ali.zhdanov@bistudio.com>
2017-06-23 17:35:26 +0200

    Added: milligram framework for nice typography styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import 'normalize.css/normalize.css';
+import 'milligram/dist/milligram.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
